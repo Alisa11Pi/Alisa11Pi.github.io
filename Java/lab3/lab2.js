1e4d15d23d33488c7c671237d7e717718a6c6339
Alisa11Pi <126443787+Alisa11Pi@users.noreply.github.com>
2023-06-06 20:50:56 +0300

    Update lab2.js

--- Java/lab3/lab2.js
+++ Java/lab3/lab2.js
@@ -1,28 +1,42 @@
 "use strict";
+
 /**
- * 
- * @param {*} x 
- * @param {*} n 
- * @returns 
+ * Функция, возвращающая x в степени n (n - целое число)
+ * @param {number} x - запрашиваемое числ, котоое возводится в степень n
+ * @param {number} n - степень, в которую возводится x
+ * @returns {number} - значение x, вовзведенное в степень 
  */
+
 function pow(x, n) {
-    /*return x**n;*/
-    if (n==0){ return 1
-    } else if (n>0){
-    for(let i=1; i<n; i++){
-        x*=x
-    }
-    return x
-    }else return 1 / pow(x, -n);
+    if (n == 0) {
+        return 1
+    } else if (n > 0) {
+        for (let i = 1; i < n; i++) {
+            x *= x
+        }
+        return x
+    } else return 1 / pow(x, -n);
 }
+
+/**
+ * вычисляет сумму чисел от 1 до n включительно
+ * @param {number} n - число, до которого необходимо рассчитать сумму
+ * @returns {number} - сумму всех чисел от n до 1 
+ */
 
 function sumTo(n) {
     if (n == 1) {
         return 1;
     } else {
-        return n + sumTo(n-1);
+        return n + sumTo(n - 1);
     }
 }
+
+/**
+ * возвращает факториал числа n!
+ * @param {number} n - число, для которого вычисляется факториал 
+ * @returns {BigInt} - Факториал заданного числа в виде BigInt
+ */
 
 function factorial(n) {
     if (n === 0 || n === 1) {
@@ -32,19 +46,52 @@ function factorial(n) {
     }
 }
 
+/**
+ * для натурального n возвращает массив, заполненный числами Фибоначчи до n-го (не включая его)
+ * @param {number} n -  число, до которого должен быть вычислен массив чисел Фибоначчи
+ * @returns {array} - массив чисел Фибоначчи с точностью до заданного числа
+ */
+
 export function fibs(n) {
-    let fibArr = [0n, 1n]; //создает массив "fibArr", который представляет собой последовательность Фибоначчи из первых n чисел
-    for (let i = 2; i < n; i++) { //вычисляются следующие (n-2) элемента массива, используя формулу последовательности Фибоначчи
-        fibArr.push(fibArr[i-1] + fibArr[i-2]);//метод push добавляет значения конец массива
+    let fibArr = [0n, 1n];
+    for (let i = 2; i < n; i++) {
+        fibArr.push(fibArr[i - 1] + fibArr[i - 2]);
     }
     return fibArr;
 }
 
+/**
+ * принимает целочисленное значение x и возвращает анонимную функцию
+ * @param {number} x - значение, с которым нужно сравнить
+ * @returns {function} -  функция, которая принимает входное значение и возвращает логическое значение
+ */
+
 function compare(x) {
-    return function(y) {
-        if(y > x) {
+    return function (y) {
+        if (y > x) {
             return true;
-        } else if(y < x) {
+        } else if (y < x) {
+            return false;
+        } else {
+            return null;
+        }
+    }
+}
+
+/**
+ * возвращает сумму всех своих аргументов
+ * @param  {...number} args - значения дя суммирования
+ * @returns {number} -  общая сумма всех аргументов
+ */
+
+function sum(...args) {
+    return args.reduce((total, current) => total + current, 0);
+}
+function compare(x) {
+    return function (y) {
+        if (y > x) {
+            return true;
+        } else if (y < x) {
             return false;
         } else {
             return null;
